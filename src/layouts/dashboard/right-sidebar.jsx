d641c5c11221dbbd784703ab455e5c339542180b
Ahmet Eren Çelik <81825344+ahmeterenc@users.noreply.github.com>
2024-09-09 15:22:36 +0300

    right bar updated

--- src/layouts/dashboard/right-sidebar.jsx
+++ src/layouts/dashboard/right-sidebar.jsx
@@ -1,5 +1,10 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 export default function RightSidebar() {
   return (
@@ -10,12 +15,60 @@ export default function RightSidebar() {
         borderLeft: '1px solid',
         borderColor: 'divider',
         p: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
       }}
     >
-      <Typography variant="h6" gutterBottom>
-        Sağ Sidebar
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar sx={{
+                width: 220,
+      }} />
+    </LocalizationProvider>
+
+      <Button variant="contained" sx={{ mt: 2, width: '100%' }}>
+        Bugün
+      </Button>
+
+      <Stack spacing={1} sx={{ mt: 2, width: '100%' }}>
+        <Button variant="contained" sx={{ bgcolor: '#006994', color: '#fff' }}>
+          F4 Bay Satış
+        </Button>
+        <Button variant="contained" sx={{ bgcolor: '#D90076', color: '#fff' }}>
+          F5 Bayan Satış
+        </Button>
+        <Button variant="contained" sx={{ bgcolor: '#009ACD', color: '#fff' }}>
+          F6 Bay Rez.
+        </Button>
+        <Button variant="contained" sx={{ bgcolor: '#FF5C77', color: '#fff' }}>
+          F7 Bayan Rez.
+        </Button>
+        <Button variant="outlined">Tüm Kısayollar İçin Tıklayın</Button>
+      </Stack>
+
+      <Typography variant="h6" sx={{ mt: 2 }}>
+        14:47
       </Typography>
-      <Typography>Burada sağ sidebar içeriği yer alacak.</Typography>
+      <Typography variant="body2">
+        Öz Has Bingöl
+      </Typography>
+      <Typography variant="body2">
+        ELAZIĞ: GEÇİCİ
+      </Typography>
+      <Typography variant="body2" sx={{ mt: 1 }}>
+        IP adresi: 193.255.125.65
+      </Typography>
+
+      <Typography variant="body2" sx={{ mt: 2 }}>
+        ORS Teknik Destek Hattı: 0212 963 01 02
+      </Typography>
+      <Typography variant="body2">
+        obilet.com çağrı merkezi:
+      </Typography>
+
+      <Button variant="contained" color="error" sx={{ mt: 2 }}>
+        Canlı Destek
+      </Button>
     </Box>
   );
 }
